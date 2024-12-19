149c6784c6ea1402298d428866b341c6f3f58b5a
Divyanshu Tripathi <divyanshutripathi@ip-10-90-0-29.ap-south-1.compute.internal>
2024-12-19 17:10:43 +0530

    Add ESLint configuration file

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,36 @@
-module.exports = [{
-    env: {
-      browser: true,
-      es2021: true
-    },
-    extends: [
-      'standard',
-      'eslint:recommended'
-    ],
-    parserOptions: {
-      ecmaVersion: 12,
-      sourceType: 'module'
+import globals from "globals";
+
+export default [
+  {
+    files: ["**/*.js", "**/*.jsx"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: {
+        ...globals.browser, // Add browser globals
+      },
     },
     rules: {
-      'no-console': 'warn',
-      'no-eval': 'error',
-      'no-shadow': 'warn',
-      'prefer-const': 'warn',
-      'no-var': 'error',
-      'no-duplicate-imports': 'warn',
-      'quote-props': 'off',
-      'camelcase': 'warn',
-      'no-multi-assign': 'warn',
-      'no-mixed-operators': 'warn',
-      'new-cap': 'error',
-      'object-shorthand': 'warn',
-      'prefer-template': 'warn',
-      'max-params': ['warn', 4],
-      'id-length': ['warn', { min: 2, max: 40 }],
-      'no-else-return': 'warn',
-      'default-param-last': 'warn',
-      'no-undef': 'warn',
-      'no-unused-vars': 'error',
-      'semi': [1, 'never']
-    }
-  }
-  ]
+      "no-console": "warn",
+      "no-eval": "error",
+      "no-shadow": "warn",
+      "prefer-const": "warn",
+      "no-var": "error",
+      "no-duplicate-imports": "warn",
+      "quote-props": "off",
+      camelcase: "warn",
+      "no-multi-assign": "warn",
+      "no-mixed-operators": "warn",
+      "new-cap": "error",
+      "object-shorthand": "warn",
+      "prefer-template": "warn",
+      "max-params": ["warn", 4],
+      "id-length": ["warn", { min: 2, max: 40 }],
+      "no-else-return": "warn",
+      "default-param-last": "warn",
+      "no-undef": "warn",
+      "no-unused-vars": "error",
+      semi: ["warn", "never"],
+    },
+  },
+];
