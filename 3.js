0a33d481079693d74a83379a18c6fc50bfc3808b
Divyanshu Tripathi <divyanshutripathi@ip-10-90-0-29.ap-south-1.compute.internal>
2024-12-19 17:54:09 +0530

    precommit hooks

--- 3.js
+++ 3.js
@@ -9,3 +9,9 @@ const twoSum =(array, target) => {
   }
     return [];
   }
+
+
+   
+ const getFederationSocietyDetails = async id => {
+    return await MONGO_MODEL.mongoFindOne('societies', { id }, { projection: { image: 1, societyName: '$name', isOnboarded: 1, propertyType: 1, accessConfig: 1, developerId: 1, logo: 1, isMemberAdditionRestricted: 1, preApprovalDays : 1 } })
+  }
