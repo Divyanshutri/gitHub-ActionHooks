b6a199628406fe7eb5555dd2e31550f69fb03e3a
Divyanshu Tripathi <divyanshutripathi@ip-10-90-0-29.ap-south-1.compute.internal>
2024-12-19 16:04:40 +0530

    first commit

---
+++ 1.js
@@ -0,0 +1,11 @@
+const twoSum =(array, target) => {
+  let map = {};
+  for (let i = 0; i < array.length; i++) {
+let diff = target - array[i];
+if (map[diff] !== undefined) {
+return [map[diff], i];
+    }
+    map[array[i]] = i;
+}
+  return [];
+}
